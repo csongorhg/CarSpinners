3d7d876e7268a2425c7ab40863f7f7eef367b7b4
Pocok <hortobiadam@gmail.com>
2016-11-27 11:12:54 +0100

    Merge branch 'master' of D:\android\megszarabbfos with conflicts.

--- core/src/com/mygdx/game/Play/PlayStage.java
+++ core/src/com/mygdx/game/Play/PlayStage.java
@@ -2,33 +2,19 @@ package com.mygdx.game.Play;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
-import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
-import com.mygdx.game.DemoLoading.LoadingScreen;
-import com.mygdx.game.DemoMenu.ExplosionActor;
-import com.mygdx.game.DemoMenu.MenuScreen;
 import com.mygdx.game.GlobalClasses.Assets;
-import com.mygdx.game.Graphics.BreakActor;
 import com.mygdx.game.Graphics.ButtonCaller;
-import com.mygdx.game.Math.Random;
-import com.mygdx.game.Music.MusicSetter;
-import com.mygdx.game.MyBaseClasses.MyLabel;
 import com.mygdx.game.Physics.Car;
 import com.mygdx.game.MyBaseClasses.MyStage;
 import com.mygdx.game.MyBaseClasses.OneSpriteStaticActor;
 import com.mygdx.game.MyGdxGame;
 import com.mygdx.game.Physics.Line;
-import com.mygdx.game.Physics.Physics;
-import com.mygdx.game.Settings.IngameSettingsStage;
+import com.mygdx.game.Physics.Physic;
 
 import java.util.Vector;
 
@@ -56,11 +42,7 @@ public class PlayStage extends MyStage {
 
     private Car car;
     private Vector<Line> lines = new Vector();
-
-    private MyLabel kmh, policedistance, score; //pocoknak
-
-    private ExplosionActor explosionActor;
-    private float elapseTime = 0; //robbanás után időt számolja
+    private static Vector<OneSpriteStaticActor> backgrounds  = new Vector<OneSpriteStaticActor>();
 
     public PlayStage(Viewport viewport, Batch batch, MyGdxGame game) {
         super(viewport, batch, game);
@@ -79,30 +61,27 @@ public class PlayStage extends MyStage {
         settingclick = b;
         return settingclick;
     }
-    protected IngameSettingsStage settingsStage;
 
     public void init() {
         addBackEventStackListener();
-
-
-        settingsStage = new IngameSettingsStage(new ExtendViewport(270,480,new OrthographicCamera(270/2,480/2)), getBatch(), game);
-
 
         resized();
 
         heart = new OneSpriteStaticActor[Car.maxheart]; //szivek
 
         //út
-        float blockHeight = (((ExtendViewport)getViewport()).getMinWorldHeight());
+        float nowHeight = 0;
 
-        for(int i = 0; i<4 ;i++){
-            oneSpriteStaticActor = new OneSpriteStaticActor(Assets.manager.get(Assets.ROAD_BLOCK));
-            float arany = (((ExtendViewport)getViewport()).getMinWorldWidth())/oneSpriteStaticActor.getWidth();
-            oneSpriteStaticActor.setSize(oneSpriteStaticActor.getWidth()*arany,oneSpriteStaticActor.getHeight()*arany);
-            blockHeight -= oneSpriteStaticActor.getHeight();
-            oneSpriteStaticActor.setPosition(0,blockHeight);
-            addActor(oneSpriteStaticActor);
-            oneSpriteStaticActor.setZIndex(0);
+        backgrounds = new Vector<OneSpriteStaticActor>();
+
+        for(int i = 0; i<5 ;i++){
+            OneSpriteStaticActor road = new OneSpriteStaticActor(Assets.manager.get(Assets.ROAD_BLOCK));
+            float arany = (((ExtendViewport)getViewport()).getMinWorldWidth())/road.getWidth();
+            road.setSize(road.getWidth()*arany,road.getHeight()*arany);
+            nowHeight += road.getHeight();
+            road.setPosition(0,nowHeight);
+            backgrounds.add(road);
+            addActor(road);
         }
 
         //fogaskerék
@@ -115,20 +94,27 @@ public class PlayStage extends MyStage {
             public void clicked(InputEvent event, float x, float y) {
                 super.clicked(event, x, y);
 
+                //TESZT szívcsökkentés
+                if (currentHeart < Car.maxheart) {
+                    OneSpriteStaticActor emptyHeart = new OneSpriteStaticActor(Assets.manager.get(Assets.NOHEART));
+                    emptyHeart.setPosition(heart[currentHeart].getX(), heart[currentHeart].getY());
+                    emptyHeart.setSize(heart[currentHeart].getWidth(), heart[currentHeart].getHeight());
+                    heart[currentHeart].remove();
+                    addActor(emptyHeart);
+                    currentHeart++;
+                }
+                //TESZT szívcsökkentés
 
-                //setSettingclick(true);
-                settingsStage.Show();
+                setSettingclick(true);
             }
         });
 
         addActor(textButton5);
-        textButton5.setZIndex(Integer.MAX_VALUE);
 
         OneSpriteStaticActor felulet = new OneSpriteStaticActor(Assets.manager.get(Assets.ROAD_MENU));
         float arany = width/felulet.getWidth();
         felulet.setSize(felulet.getWidth()*arany,felulet.getHeight()*arany);
         addActor(felulet);
-        felulet.setZIndex(Integer.MAX_VALUE);
 
        //gázpedál
         p = new ButtonCaller("", Assets.GAZ_ICON);
@@ -149,7 +135,6 @@ public class PlayStage extends MyStage {
         });
 
         addActor(p);
-        p.setZIndex(Integer.MAX_VALUE);
 
         //fékpedál
         f = new ButtonCaller("", Assets.FEK_ICON);
@@ -169,11 +154,9 @@ public class PlayStage extends MyStage {
         });
 
         addActor(f);
-        f.setZIndex(Integer.MAX_VALUE);
 
         car = new Car(width/2 - Car.carTexture.getPaint().getWidth()/2,heigth/10,Car.carTexture.getPaint().getWidth(),Car.carTexture.getPaint().getHeight());
         addActor(car.carActor);
-        car.carActor.setZIndex(Integer.MAX_VALUE);
         car.carActor.act(Gdx.graphics.getDeltaTime());
 
         //szivek
@@ -186,140 +169,45 @@ public class PlayStage extends MyStage {
             heart[i].setX(width-x);
             heart[i].setY(heigth-30);
             addActor(heart[i]);
-            heart[i].setZIndex(Integer.MAX_VALUE);
             x-=30;
         }
-
-
-        Label.LabelStyle style;
-        style = new com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle();
-        style.fontColor = Color.WHITE;
-
-        //átméretezés
-        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("c64.ttf"));
-        FreeTypeFontGenerator.FreeTypeFontParameter meret = new FreeTypeFontGenerator.FreeTypeFontParameter();
-        meret.size = 10;
-        meret.characters = Assets.CHARS;
-        BitmapFont font = generator.generateFont(meret);
-        generator.dispose();
-        style.font = font;
-        //átméretezés vége
-
-
-        //kilóméter/óra
-        kmh = new MyLabel("asd1", style);
-        //myLabel.setPosition(getViewport().getWorldWidth()/(590/100),getViewport().getWorldHeight()/24);
-        kmh.setPosition(75,19);
-        addActor(kmh);
-
-        //rendőr távolság
-        policedistance = new MyLabel("asd2",style);
-        policedistance.setPosition(75,4);
-        addActor(policedistance);
-
-        //score
-        score = new MyLabel("score",game.getLabelStyle());
-        score.setPosition(116,7);
-        addActor(score);
-
-
-
     }
 
     @Override
     public void act(float delta) {
         super.act(delta);
-        carPhysics();
+        carPysic();
         linePhysic();
-        crashPhysic();
-        strings();
-        if (currentHeart == 5) {
-            explosion(delta);
-        }
-        if(settingsStage.isB())new MusicSetter(new Random(1,5).getGenNumber());
-        settingsStage.act(delta);
+        backgroundPhysic();
     }
 
-    @Override
-    public void draw() {
-        super.draw();
-
-        settingsStage.draw();
-    }
-
-    private void explosion(float delta) {
-        elapseTime += delta;
-        if (elapseTime > 3f) {
-            explosionActor.remove();
-            //game.setScreen(new MenuScreen(game)); //itt akad ki
-            game.setScreenBackByStackPop();
-        }
-    }
-
-    private void strings() {
-        kmh.setText(Physics.round(Physics.carspeed*10)+" km/h");
-        policedistance.setText(Physics.round(Physics.policedis)+" m");
-        score.setText("00000");
-    }
-
-    private void crashPhysic() {
-        Line l = lines.get(0);
-        for (int i=0; i<l.blocks.length;i++){
-            if(Physics.hit(l.blocks[i].actor,car.carActor)){
-                if(l.blocks[i].getWeight() == 1) {
-                    if (currentHeart < Car.maxheart) {
-                        car.damage();
-                        OneSpriteStaticActor emptyHeart = new OneSpriteStaticActor(Assets.manager.get(Assets.NOHEART));
-                        emptyHeart.setPosition(heart[currentHeart].getX(), heart[currentHeart].getY());
-                        emptyHeart.setSize(heart[currentHeart].getWidth(), heart[currentHeart].getHeight());
-                        heart[currentHeart].remove();
-                        addActor(emptyHeart);
-                        currentHeart++;
-                        if (currentHeart == 5) {
-                            car.carActor.remove();
-                            explosionActor = new ExplosionActor();
-                            explosionActor.setPosition(car.carActor.getX() + car.carActor.getWidth()/2 - explosionActor.getWidth()/2,
-                                    car.carActor.getY() - car.carActor.getHeight()/2);
-                            addActor(explosionActor);
-                        }
-                    }
-                    l.blocks[i].setWeight(0);
-                    l.blocks[i].actor.remove();
-                }
-            }
+    private void backgroundPhysic() {
+        //if(backgrounds.get(0))
+        for (int i = 0; i < backgrounds.size(); i++){
+            backgrounds.get(i).setPosition(backgrounds.get(i).getX(),backgrounds.get(i).getY()-Physic.carspeed);
         }
     }
 
     private void linePhysic(){
-        float speed = Physics.carspeed;
+        float speed = Physic.carspeed;
         try{
             if(!fekisdown && !gazisdown) speed *= 0.99;
-            else if(fekisdown){
-                if(speed >= 0 && speed <= 1)speed -= 1.01;
-                speed *= Physics.breakpower;
-                if (speed>1) {
-                    car.breaking();
-                }
+            else if(fekisdown) {
+                speed -= 0.01f;
+                speed *= Physic.breakpower;
             }
-            else if(gazisdown){
-                if(speed >= 0 && speed <= 1)speed += 1.01;
-                speed *= Physics.acceleration;
+            else if(gazisdown) {
+                speed += 0.01f;
+                speed *= Physic.acceleration;
             }
 
-            if(speed < Physics.MINcarspeed) speed = Physics.MINcarspeed;
-            else if(speed > Physics.MAXcarspeed) speed = Physics.MAXcarspeed;
+            if(speed < Physic.MINcarspeed) speed = Physic.MINcarspeed;
+            else if(speed > Physic.MAXcarspeed) speed = Physic.MAXcarspeed;
 
-            Physics.carspeed = speed;
-
-            Physics.policedis += speed - Physics.policespeed;
+            Physic.carspeed = speed;
 
             for (int i = 0; i < lines.size(); i++){
                 lines.get(i).addHeight(speed);
-            }
-            for (Actor a:getActors()) {
-                if (a instanceof BreakActor){
-                    a.setY(a.getY()-speed);
-                }
             }
             if(lines.get(0).heightpoz+lines.get(0).size < 0){
                 removeLine();
@@ -345,20 +233,14 @@ public class PlayStage extends MyStage {
         Line l = new Line(width,heigth);
         for (int i = 0; i < 3; i++){
             addActor(l.blocks[i].actor);
-            l.blocks[i].actor.setZIndex(5);
         }
         lines.add(l);
-
     }
 
-    private void carPhysics(){
-        car.carActor.setPosition(car.carActor.getX()-(Gdx.input.getAccelerometerX()/3),car.carActor.getY());
-        if(car.carActor.getX()+car.carActor.getWidth() > width/5*4){
-            car.carActor.setPosition(width/5*4-car.carActor.getWidth(),car.carActor.getY());
-        }
-        if(car.carActor.getX()< width/5){
-            car.carActor.setPosition(width/5,car.carActor.getY());
-        }
+    private void carPysic(){
+        car.carActor.setPosition(car.carActor.getX()-(Gdx.input.getAccelerometerX()/4),car.carActor.getY());
+        if(car.carActor.getX()+car.carActor.getWidth() > width/5*4) car.carActor.setPosition(width/5*4-car.carActor.getWidth(),car.carActor.getY());
+        if(car.carActor.getX()< width/5) car.carActor.setPosition(width/5,car.carActor.getY());
 
 
     }
@@ -369,11 +251,5 @@ public class PlayStage extends MyStage {
         width = (((ExtendViewport)getViewport()).getMinWorldWidth());
         heigth = (((ExtendViewport)getViewport()).getMinWorldHeight());
 
-    }
-
-    @Override
-    public void dispose() {
-        settingsStage.dispose();
-        super.dispose();
     }
 }
