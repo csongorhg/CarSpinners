504c83dad6c2dc6baf7a69c765a044d1209d8aa9
Kicsi <kissnorbi9935@gmail.com>
2016-12-09 10:30:59 +0100

    csetteg még mindig csak most a extrascreeen

--- core/src/com/mygdx/game/Extras/ExtrasScreen.java
+++ core/src/com/mygdx/game/Extras/ExtrasScreen.java
@@ -3,7 +3,12 @@ package com.mygdx.game.Extras;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.mygdx.game.DemoMenu.MenuStage;
+import com.mygdx.game.GlobalClasses.Assets;
 import com.mygdx.game.MyBaseClasses.MyScreen;
+import com.mygdx.game.MyBaseClasses.MyStage;
+import com.mygdx.game.MyBaseClasses.OneSpriteStaticActor;
 import com.mygdx.game.MyGdxGame;
 
 /**
@@ -12,6 +17,8 @@ import com.mygdx.game.MyGdxGame;
 public class ExtrasScreen extends MyScreen {
     protected ExtrasStage extrasStage;
 
+    protected MyStage bgStage;
+
     public ExtrasScreen(MyGdxGame game) {
         super(game);
     }
@@ -19,16 +26,31 @@ public class ExtrasScreen extends MyScreen {
     @Override
     public void render(float delta) {
         super.render(delta);
+
+        bgStage.act(delta);
+        bgStage.draw();
         extrasStage.act(delta);
         extrasStage.draw();
 
     }
 
+
     @Override
     public void init() {
-        r = 1;
-        g = 0.5f;
-        b = 0.3f;
+        bgStage = new MyStage(new StretchViewport(72,128, new OrthographicCamera(72,128)), spriteBatch, game) {
+
+            private OneSpriteStaticActor backGroudActor;
+
+            @Override
+            public void init() {
+                addActor(backGroudActor = new OneSpriteStaticActor(Assets.manager.get(Assets.BACKGROUND_TEXTURE)));
+            }
+
+            @Override
+            protected void resized() {
+
+            }
+        };
         extrasStage = new ExtrasStage(new ExtendViewport(270,480,new OrthographicCamera(270,480)), spriteBatch, game);
         Gdx.input.setInputProcessor(extrasStage);
     }
